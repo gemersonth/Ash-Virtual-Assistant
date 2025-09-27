a182e38bc0d127b169c02ddf4f0ed02cc889035d
Gemerson <gemerson28thiago.dev@gmail.com>
2025-09-27 13:21:14 -0300

    speaker implemented

---
+++ speaker.py
@@ -0,0 +1,16 @@
+import pyttsx3
+
+class Speaker:
+    def __init__(self):
+        self.engine = pyttsx3.init()
+        self.engine.setProperty("rate", 210)
+        self.engine.setProperty("volume", 1.0)
+        self.engine.setProperty("voice", 'pt')
+    
+    def say(self, text):
+        self.engine.say(text)
+        self.engine.runAndWait()
+
+if __name__ == '__main__':
+    s = Speaker()
+    s.say("Olá")
