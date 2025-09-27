d89e6a1e8c9738db125d8999fe1ccd0b264d8aa3
Gemerson <gemerson28thiago.dev@gmail.com>
2025-09-27 11:40:04 -0300

    optimized voice recognition code

--- voice_recognition.py
+++ voice_recognition.py
@@ -13,18 +13,12 @@ class Recognize():
             print("NÃO FOI POSSÍVEL ENCONTRAR: model_br")
             sys.exit(1)
         
-        # Ler e executa o modelo em pt-BR
+        # Carregar o modelo em pt-BR
         self.model = Model("model_br")
         self.rec = KaldiRecognizer(self.model, 16000)
         self.p = pyaudio.PyAudio()
         self.stream = self.p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=4096)
       
-    
-    def ignore_arguments(self):
-        arguments = ('', 'none', 'None', ' ', None,
-        'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's',
-        'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ç', 'z', 'x', 'c', 'v', 'b', 'n', 'm')
-        return arguments
     
     def capture(self):
         self.rec.pause_threshold = 2
@@ -37,15 +31,10 @@ class Recognize():
         except:
             return None
         return text
-    
-    def run(self):
-        ignore = self.ignore_arguments()
-        while True:
-            I = self.capture().lower()
-            print(I)
-                    
             
             
 if __name__ == '__main__':
     a = Recognize()
-    a.run()
+    while True:
+        I = a.capture().lower()
+        print(I)
