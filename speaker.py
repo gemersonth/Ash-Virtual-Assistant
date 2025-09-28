4ba05c6c90c37ebedef9dd82f2ea888a30b43955
Gemerson <gemerson28thiago.dev@gmail.com>
2025-09-28 00:48:53 -0300

    refresh code

--- speaker.py
+++ speaker.py
@@ -3,7 +3,7 @@ import pyttsx3
 class Speaker:
     def __init__(self):
         self.engine = pyttsx3.init()
-        self.engine.setProperty("rate", 210)
+        self.engine.setProperty("rate", 170)
         self.engine.setProperty("volume", 1.0)
         self.engine.setProperty("voice", 'pt')
     
--- voice_recognition.py
+++ voice_recognition.py
@@ -4,17 +4,18 @@ import pyaudio
 import os
 import json
 
+PATH = "vosk_model_br"
+
 class Recognize():
     def __init__(self):
-
         # Verificar se a pasta 'pacote-br' existe
-        if not os.path.exists("model_br"):
+        if not os.path.exists(PATH):
             raise FileNotFoundError("ARQUIVO 'model_br' NÃO ENCONTRADO")
             print("NÃO FOI POSSÍVEL ENCONTRAR: model_br")
             sys.exit(1)
         
         # Carregar o modelo em pt-BR
-        self.model = Model("model_br")
+        self.model = Model(PATH)
         self.rec = KaldiRecognizer(self.model, 16000)
         self.p = pyaudio.PyAudio()
         self.stream = self.p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=4096)
@@ -22,7 +23,7 @@ class Recognize():
     
     def capture(self):
         self.rec.pause_threshold = 2
-        self.data = self.stream.read(40000)
+        self.data = self.stream.read(40000, exception_on_overflow=False)
         self.rec.AcceptWaveform(self.data)
         try:
             result = self.rec.Result()
@@ -34,7 +35,7 @@ class Recognize():
             
             
 if __name__ == '__main__':
-    a = Recognize()
+    r = Recognize()
     while True:
-        I = a.capture().lower()
+        I = r.capture().lower()
         print(I)
